3254aa283546057052437147dd9c9e21866aa0ba
Max Liu <renjing.liu@mail.utoronto.ca>
2021-01-28 13:14:00 +0800

    R script

---
+++ script/Paper_code.R
@@ -0,0 +1,12 @@
+#### Preamble ####
+# Purpose: Use opendatatoronto to get ... data
+# Author: Renjing Liu (Max)
+# Contact: renjing.liu@mail.utoronto.ca
+# Data: 28th January 2021
+# Pre-requisites: None
+# TODOs: 
+
+#### Workspace set-up ####
+# install.packages("opendatatoronto")
+library(opendatatoronto)
+
